4befd551a75495a66e8dd97f40ece6b6391f4c02
Michael Täge <mictaege@googlemail.com>
2021-05-11 09:47:04 +0200

    alphabetical, hierarchical and tagged overviews added

--- src/test/java/com/github/mictaege/arete_gradle_test/XDescribeSpec.java
+++ src/test/java/com/github/mictaege/arete_gradle_test/XDescribeSpec.java
@@ -1,9 +1,14 @@
 package com.github.mictaege.arete_gradle_test;
 
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+
 import com.github.mictaege.arete.Describe;
 import com.github.mictaege.arete.ItShould;
 import com.github.mictaege.arete.Spec;
 
+@Tag("fast")
+@Tag("internal")
 @Spec class XDescribeSpec {
 
     @Describe class ADescription {
