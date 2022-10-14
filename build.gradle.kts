0c86b60f0c9e1b12d53d3e24b5e2b0130cc1b198
Michael Taege <Michael.Taege@zitecs.de>
2022-10-14 14:47:55 +0200

    Update Development Environment

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
+    id("com.github.mictaege.arete") version "2022.2"
+    
+    `maven-publish`
+    signing
+}
+
+group = "com.github.mictaege"
+version = "2022.2"
+
+tasks.wrapper {
+    gradleVersion = "7.5.1"
+    distributionType = Wrapper.DistributionType.ALL
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    testImplementation("com.github.mictaege:arete:2022.2")
+    testImplementation("org.hamcrest:hamcrest:2.2")
+    testImplementation("com.google.guava:guava:30.1.1-jre")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
