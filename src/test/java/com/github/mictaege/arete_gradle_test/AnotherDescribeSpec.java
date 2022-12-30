9f0a3bb0c143357a9651042c0690368fdab6ae31
Michael Taege <mictaege@googlemail.com>
2022-12-30 14:47:37 +0100

    #6 Link specifications and scenarios with each other

--- src/test/java/com/github/mictaege/arete_gradle_test/AnotherDescribeSpec.java
+++ src/test/java/com/github/mictaege/arete_gradle_test/AnotherDescribeSpec.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import com.github.mictaege.arete.Describe;
 import com.github.mictaege.arete.ItShould;
 import com.github.mictaege.arete.ScreenshotExtension;
+import com.github.mictaege.arete.SeeAlso;
 import com.github.mictaege.arete.Spec;
 
 @Spec class AnotherDescribeSpec {
@@ -19,6 +20,7 @@ import com.github.mictaege.arete.Spec;
     @RegisterExtension
     public ScreenshotExtension screenshots = new ScreenshotExtension(new DummyScreenshotTaker());
 
+    @SeeAlso(ArithmeticCalculationsSpec.MyOtherFeature.MyOtherScenario.MyScenario.class)
     @Describe class ADescription {
         @ItShould void doSomething() {
         }
