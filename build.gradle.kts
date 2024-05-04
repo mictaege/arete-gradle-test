f5f2f5569885b4bf687efcec018ffe5df6345b6e
Michael Taege <mictaege@googlemail.com>
2024-05-04 08:24:57 +0200

    Update Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
     id("io.github.mictaege.arete") version "2024.1"
     `maven-publish`
     signing
@@ -22,9 +22,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     testImplementation("io.github.mictaege:arete:2024.1")
     testImplementation("org.hamcrest:hamcrest:2.2")
-    testImplementation("com.google.guava:guava:31.1-jre")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+    testImplementation("com.google.guava:guava:33.2.0-jre")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 }
 
 tasks.test {
