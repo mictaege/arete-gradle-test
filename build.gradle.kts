78e7f910f2db22e011c1b0a4c757501a6f3055df
Michael Taege <Michael.Taege@zitecs.de>
2026-01-05 22:03:18 +0100

    #21 Update all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import com.github.mictaege.arete_gradle.CatppuccinFrappeColors
 
 plugins {
     java
-    id("org.jetbrains.kotlin.jvm") version "1.9.22"
-    id("io.github.mictaege.arete") version "2024.2"
+    id("org.jetbrains.kotlin.jvm") version "2.3.0"
+    id("io.github.mictaege.arete") version "2025.1-rc1"
     `maven-publish`
     signing
 }
 
 group = "io.github.mictaege"
-version = "2024.2"
+version = "2025.1-rc1"
 
 tasks.wrapper {
     gradleVersion = "8.7"
@@ -22,11 +22,11 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
-    testImplementation("io.github.mictaege:arete:2024.2")
-    testImplementation("org.hamcrest:hamcrest:2.2")
-    testImplementation("com.google.guava:guava:33.2.0-jre")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testImplementation("io.github.mictaege:arete:2025.1-rc1")
+    testImplementation("org.hamcrest:hamcrest:3.0")
+    testImplementation("com.google.guava:guava:33.5.0-jre")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.4")
 }
 
 arete {
