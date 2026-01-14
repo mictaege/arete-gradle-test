ddbf68ac2066f708557f4de3c09139062230aa3d
Michael Taege <Michael.Taege@zitecs.de>
2026-01-14 13:10:06 +0100

    #21 Update all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,13 +20,19 @@ repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     testImplementation("io.github.mictaege:arete:2025.1-rc1")
     testImplementation("org.hamcrest:hamcrest:3.0")
     testImplementation("com.google.guava:guava:33.5.0-jre")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.11.4")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:6.0.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.2")
 }
 
 arete {
